0ce6b1a7392a908e725637da1893a7f7d1888233
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-12-19 12:59:38 +0530

    NewChat Change

--- client/src/Components/Buttons/NewChat.tsx
+++ client/src/Components/Buttons/NewChat.tsx
@@ -1,10 +1,31 @@
 import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { db } from "../../../FireBase/FireBase";
 
 const NewChat = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const document = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        messages: [],
+        userId: session?.user?.email!,
+        userName: session?.user?.name,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    router.push(`/chat/${document.id}`);
+  };
+
   return (
-    <div className="border-gray-700 border chatRow">
+    <div className="border-gray-700 border chatRow" onClick={createNewChat}>
       <FontAwesomeIcon icon={faAdd} className="h-4 w-4" />
       <p>New Chat</p>
     </div>
