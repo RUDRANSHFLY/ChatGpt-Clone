1ebcac408e24d1000bc3e70a89509b0812f7619d
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2023-12-19 11:28:44 +0530

    ChatInput Add

---
+++ client/src/Components/ui/ChatInput.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { PaperAirplaneIcon } from "@heroicons/react/20/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import React from "react";
+import { useState } from "react";
+import { db } from "../../../FireBase/FireBase";
+
+type Props = {
+  chatId: string;
+};
+
+const ChatInput = ({ chatId }: Props) => {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!prompt) {
+      return;
+    }
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name!}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+  };
+
+  return (
+    <div className="bg-gray-600/50 text-gray-400 rounded-lg text-sm focus:outline-none mb-10 ml-5 mr-5">
+      <form className="p-5 space-x-5 flex" onSubmit={sendMessage}>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={prompt}
+          onChange={(e) => {
+            setPrompt(e.target.value);
+          }}
+          placeholder="Type your message here..."
+          className="text-white bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+        />
+        <button
+          disabled={!prompt || !session}
+          className={`bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-black disabled:cursor-not-allowed`}
+        >
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatInput;
