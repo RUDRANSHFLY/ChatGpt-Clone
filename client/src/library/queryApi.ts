9cd6c2ec5d05678d4ebaadf4d823c99ebfddd445
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-15 09:00:25 +0530

    Testing Api

--- client/src/library/queryApi.ts
+++ client/src/library/queryApi.ts
@@ -1,8 +1,11 @@
 import OpenAI from "openai";
 import { mainGpt } from "./chatgpt";
+import { getModels } from "makeRequest";
 
 
 const query = async (prompt: string  ,chatID : string  , model : string) => {
+   console.log("CHECKING MODEL REQUEST");
+   await getModels();
    return await mainGpt( prompt , chatID , model );
 }
 
