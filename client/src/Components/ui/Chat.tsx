af7171084ce9ace1990a2701dbf19481f4341839
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-15 08:59:35 +0530

    Input Decoration for More Visual Add

--- client/src/Components/ui/Chat.tsx
+++ client/src/Components/ui/Chat.tsx
@@ -1,13 +1,49 @@
+"use client";
+
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 import React from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../FireBase/FireBase";
+import Message from "model/Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   chatId: string;
 };
 
 const Chat = ({ chatId }: Props) => {
+  const { data: session } = useSession();
+
+  const [messages, loading, error] = useCollection(
+    session &&
+      query(
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
+        orderBy("createdAt", "asc")
+      )
+  );
+
   return (
-    <div className="flex-1">
-      <h1>Chat</h1>
+    <div className="flex-1 overflow-y-auto">
+      {messages?.empty && (
+        <>
+          <p className="mt-10 text-center text-white">
+            Type a prompt in below to get started!
+          </p>
+          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
+        </>
+      )}
+
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
     </div>
   );
 };
