d48540b6b0634af87c8743a6251ab8ca0c289d69
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-15 20:33:09 +0530

    Model Api Create

---
+++ client/src/pages/api/getEngine.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest , NextApiResponse } from "next";
+import { mainGpt } from "library/chatgpt";
+import { NextResponse } from "next/server";
+import { getModels } from "makeRequest";
+
+
+type Option = {
+    value : string ;
+    label : string ;
+};
+
+
+
+type Data = {
+    modelOptions : Option[];
+};
+
+
+export default async function handler(
+    req : NextApiRequest,
+    res : NextApiResponse<Data>
+){
+
+
+    const models = await getModels() ;
+
+
+    console.log(models);
+    
+
+
+    const modelOptions = models.data.map((model: { id: string; owned_by: string; }) => ({
+        value : model.id,
+        label : model.owned_by,
+    }));
+
+    res.status(200).json({
+        modelOptions,
+    });
+
+
+
+}
