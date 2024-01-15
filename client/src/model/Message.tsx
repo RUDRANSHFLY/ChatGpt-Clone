2509c12cf9694d9a919a8b30b5a70980c8f5d109
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-15 08:54:46 +0530

    Message Model Page Add

---
+++ client/src/model/Message.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { DocumentData } from "firebase-admin/firestore";
+
+type Props = {
+  message: DocumentData;
+};
+
+const Message = ({ message }: Props) => {
+  console.log(message.user.name);
+
+  const isChatGPT = message.user.name == "ChatGPT";
+  console.log(isChatGPT);
+
+  return (
+    <div className={`py-10  text-white ${isChatGPT && "bg-[#434654]"}`}>
+      <div className="flex space-x-5 max-w-2xl mx-auto">
+        <img src={message.user.avatar} alt="" className="h-8 w-8 " />
+        <p className="pt-1 text-sm">{message.text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
